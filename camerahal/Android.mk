6c8a63c6984b1c85aa3d60fe771436c1686c3379
QuietStorm1785 <QuietStorm1785@gmail.com>
2013-02-11 00:29:13 -0500

    updated camerahal

--- camerahal/Android.mk
+++ camerahal/Android.mk
@@ -1,55 +1,36 @@
-# Copyright (C) 2012 zathrasorama@gmail.com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+ifeq ($(BOARD_USES_HTC_CAMERA),true) 
+#|| ifeq ($(TARGET_BOARD_PLATFORM),msm7x30) # From htc http://review.cyanogenmod.com/#/c/12793/12/camerahal/Android.mk
 
 LOCAL_PATH:= $(call my-dir)
-	
+
 include $(CLEAR_VARS)
-	
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
-LOCAL_CFLAGS := -DCONFIG_720P_CAMERA
-LOCAL_C_INCLUDES += external/jpeg \
-                    external/skia/include/core/ \
-                    frameworks/base/core/jni/android/graphics
 
 LOCAL_MODULE_TAGS := optional
-	
-LOCAL_SRC_FILES := \
-    HalModule.cpp \
-    CameraFactory.cpp \
-    Camera.cpp \
-    CameraDevice.cpp \
-    PreviewWindow.cpp \
-    CallbackNotifier.cpp \
-    MsmCamera.cpp \
-    MsmCameraDevice.cpp \
-    Memory.cpp \
-    Converters.cpp \
-    ParameterHelper.cpp \
-    JpegCompressor.cpp
 
-LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libbinder
-LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
+LOCAL_SRC_FILES := cameraHAL.cpp OverlayHtc.cpp
+LOCAL_C_INCLUDES := frameworks/av/include frameworks/base/include frameworks/native/include
+LOCAL_C_INCLUDES += hardware/libhardware/include/ hardware
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libbinder libdl libcamera
+LOCAL_SHARED_LIBRARIES += libcamera_client libui libhardware libseccameraadaptor
 LOCAL_PRELINK_MODULE := false
 
-LOCAL_SHARED_LIBRARIES += \
-        libjpeg \
-        libskia \
-        libandroid_runtime
+# hack for prebuilt
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/export_includes)
+
+ifeq ($(BOARD_HAVE_HTC_FFC), true)
+	LOCAL_CFLAGS += -DHTC_FFC
+endif
+ifeq ($(BOARD_USE_REVERSE_FFC), true)
+	LOCAL_CFLAGS += -DREVERSE_FFC
+endif
 
 include $(BUILD_SHARED_LIBRARY)
-	
+
 endif
